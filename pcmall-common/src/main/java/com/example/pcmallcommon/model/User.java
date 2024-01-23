67a6d789d9151e1d8fc37666e07ff981d84345a6
Teriri <1511736399@qq.com>
2024-01-23 18:44:06 +0800

    user实体类

--- pcmall-common/src/main/java/com/example/pcmallcommon/model/User.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/model/User.java
@@ -5,17 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@TableName("User")
+@TableName("user")
 public class User {
     private Integer id;
     private String uid;//用户号
     private String uname;//用户名
     private String password;//密码（MD5加密）
-    private Integer flag;//账号类型，0商家，1用户
-    private Integer status;//假删除，0删除了，1没删除
+    private Integer isDelete;//0正常，1删除
+    private LocalDateTime createdTime;//创建时间
+    private LocalDateTime loginTime;//最后一次登录时间
     //private byte[] image;
     //private String imageString;//头像
 }
