9f9b17b0152fa8ad88549a468bfa000aa1ab45f7
Teriri <1511736399@qq.com>
2024-01-23 18:44:15 +0800

    user dao

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/dao/IUserDao.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/dao/IUserDao.java
@@ -2,8 +2,13 @@ package com.example.pcmalluserservice.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.pcmallcommon.model.User;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IUserDao extends BaseMapper<User> {
+    @Select("SELECT * FROM user_role WHERE uid=#{uid}")
+    List<String> findUserRole(String uid);
 }
