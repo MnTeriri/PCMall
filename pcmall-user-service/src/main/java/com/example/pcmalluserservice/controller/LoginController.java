cc3c63720f012ea6df87aaada425b61833996999
Teriri <1511736399@qq.com>
2024-01-25 21:46:16 +0800

    LoginController

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/LoginController.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.example.pcmalluserservice.controller;
 
+import com.example.pcmallcommon.model.User;
+import com.example.pcmallcommon.response.ResponseResult;
 import com.example.pcmalluserservice.service.IUserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +20,15 @@ public class LoginController {
     @Autowired
     private IUserService userService;
 
-    @RequestMapping(value = "/api/login", method = {RequestMethod.GET, RequestMethod.POST})
-    public String login() {
+//    @RequestMapping(value = "/api/login", method = {RequestMethod.GET, RequestMethod.POST})
+//    public String login() {
+//        return "这是登录接口,token:" + userService.login("000000000", "123456");
+//    }
 
-        return "这是登录接口,token:" + userService.login("000000000", "123456");
+    @RequestMapping(value = "/api/login", method = {RequestMethod.GET, RequestMethod.POST})
+    public ResponseResult<User> login(String uid, String password) {
+        //判定验证码
+
+        return userService.login(uid, password);
     }
 }
