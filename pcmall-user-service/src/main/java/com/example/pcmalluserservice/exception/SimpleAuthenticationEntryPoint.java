13b53312ca219f2ca6c37a4c0c9b5e48c5144fcc
Teriri <1511736399@qq.com>
2024-01-25 21:46:54 +0800

    AuthenticationEntryPoint

---
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/exception/SimpleAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.example.pcmalluserservice.exception;
+
+import com.alibaba.fastjson2.JSON;
+import com.example.pcmallcommon.response.ResponseResult;
+import com.example.pcmallcommon.response.ResponseStatus;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+@Component
+public class SimpleAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        ResponseResult<String> message = ResponseResult.error(ResponseStatus.AUTHORIZED_ERROR);
+        log.error(message.toString());
+        writer.write(JSON.toJSONString(message));
+    }
+}
