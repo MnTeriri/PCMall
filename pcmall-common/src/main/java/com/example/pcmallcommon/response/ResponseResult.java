4d6131747b25d7541a28b5de487520b85e71df6b
Teriri <1511736399@qq.com>
2024-01-25 21:44:54 +0800

    结果返回类

--- pcmall-common/src/main/java/com/example/pcmallcommon/response/ResponseResult.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/response/ResponseResult.java
@@ -8,18 +8,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class ResponseResult<T> {
-    private ResponseStatus status;
+    private Integer code;
+    private String message;
     private T data;
 
     public static <T> ResponseResult<T> ok(T data) {
-        return new ResponseResult<>(ResponseStatus.OK, data);
+        ResponseResult<T> result = new ResponseResult<>();
+        result.setCode(ResponseStatus.OK.getCode());
+        result.setMessage(ResponseStatus.OK.getMessage());
+        result.setData(data);
+        return result;
+    }
+
+    public static <T> ResponseResult<T> ok(T data, String msg) {
+        ResponseResult<T> result = new ResponseResult<>();
+        result.setCode(ResponseStatus.OK.getCode());
+        result.setMessage(msg);
+        result.setData(data);
+        return result;
     }
 
     public static ResponseResult<String> error(ResponseStatus status) {
-        return new ResponseResult<>(status, status.getMessage());
+        return new ResponseResult<>(status.getCode(), status.getMessage(), null);
     }
 
-    public static ResponseResult<String> error(ResponseStatus status, String msg) {
-        return new ResponseResult<>(status, msg);
+    public static ResponseResult<String> error(Integer code, String msg) {
+        return new ResponseResult<>(code, msg, null);
     }
 }
