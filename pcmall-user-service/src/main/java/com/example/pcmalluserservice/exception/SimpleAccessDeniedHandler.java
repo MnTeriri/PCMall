3816116686cfe39c34984e1af109b4728dd1da1f
Teriri <1511736399@qq.com>
2024-01-25 21:46:47 +0800

    AccessDeniedHandler

---
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/exception/SimpleAccessDeniedHandler.java
@@ -0,0 +1,30 @@
+package com.example.pcmalluserservice.exception;
+
+import com.alibaba.fastjson2.JSON;
+import com.example.pcmallcommon.response.ResponseResult;
+import com.example.pcmallcommon.response.ResponseStatus;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+@Component
+public class SimpleAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        PrintWriter writer = response.getWriter();
+        ResponseResult<String> message = ResponseResult.error(ResponseStatus.FORBIDDEN_ERROR);
+        log.error(message.toString());
+        writer.write(JSON.toJSONString(message));
+    }
+}
