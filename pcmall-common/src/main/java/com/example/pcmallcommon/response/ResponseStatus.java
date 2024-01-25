1d2d598b08b94a285e1799fb4c1551bcfa251d83
Teriri <1511736399@qq.com>
2024-01-25 21:45:04 +0800

    结果返回状态枚举类

--- pcmall-common/src/main/java/com/example/pcmallcommon/response/ResponseStatus.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/response/ResponseStatus.java
@@ -1,27 +1,21 @@
 package com.example.pcmallcommon.response;
 
+import lombok.Getter;
+
+@Getter
 public enum ResponseStatus {
     OK(200, "操作成功"),
-    INTERNAL_ERROR(500000, "系统错误"),
-    BUSINESS_ERROR(500001, "业务错误"),
-    LOGIN_ERROR(500002, "账号或密码错误"),
-    NO_DATA_ERROR(500003, "没有找到数据"),
-    PARAM_ERROR(500004, "参数格式错误"),
-    AUTH_ERROR(401, "没有权限,需要登录");
+    NO_TOKEN_ERROR(500000, "没有token"),
+    TOKEN_EXPIRE_ERROR(500001, "token过期"),
+    AUTHORIZED_ERROR(401, "没有权限，需要登录"),
+    FORBIDDEN_ERROR(403, "权限不够，被拒绝");
 
-    private Integer code;
-    private String message;
+    private final Integer code;
+    private final String message;
 
     ResponseStatus(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
 
-    public Integer getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
