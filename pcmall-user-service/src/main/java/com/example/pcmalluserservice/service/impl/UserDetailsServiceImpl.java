a5b0d500ff9a90650a0adf846cd7d63c2b031ba0
Teriri <1511736399@qq.com>
2024-01-25 21:45:41 +0800

    Spring Security的UserService

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/service/impl/UserDetailsServiceImpl.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/service/impl/UserDetailsServiceImpl.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.pcmallcommon.model.LoginUser;
 import com.example.pcmallcommon.model.User;
 import com.example.pcmalluserservice.dao.IUserDao;
+import com.example.pcmalluserservice.dao.IUserRoleDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,28 +14,27 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private IUserDao userDao;
+    @Autowired
+    private IUserRoleDao userRoleDao;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         QueryWrapper<User> queryWrapper = new QueryWrapper<User>()
                 .eq("uid", username);
         User user = userDao.selectOne(queryWrapper);
-        log.debug("用户{}信息查询成功", user);
-        user.setPassword("{bcrypt}" + new BCryptPasswordEncoder().encode("123456"));
+        log.debug("用户信息查询成功！，信息为：{}", user);
+        user.setPassword("{bcrypt}" + new BCryptPasswordEncoder().encode(user.getPassword()));
         //user.setPassword(new BCryptPasswordEncoder().encode("123456"));
-        ArrayList<String> roles = new ArrayList<>();
-        roles.add("admin");
-        roles.add("user");
+        List<String> roles = userRoleDao.findUserRole(username);
         LoginUser loginUser = new LoginUser(user, roles);
-        log.debug("用户权限信息为{}", JSON.toJSONString(loginUser));
-
+        log.debug("用户权限信息为：{}", JSON.toJSONString(loginUser));
         return loginUser;
     }
 }
