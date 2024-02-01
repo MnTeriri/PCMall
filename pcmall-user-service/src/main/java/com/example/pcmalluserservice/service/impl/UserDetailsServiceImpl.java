5929b21c730fe08408449fe4b5e3d9c3fb128076
Teriri <1511736399@qq.com>
2024-02-01 20:22:53 +0800

    Spring Security的UserService

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/service/impl/UserDetailsServiceImpl.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/service/impl/UserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.pcmallcommon.model.LoginUser;
 import com.example.pcmallcommon.model.User;
+import com.example.pcmallcommon.response.ResponseStatus;
 import com.example.pcmalluserservice.dao.IUserDao;
 import com.example.pcmalluserservice.dao.IUserRoleDao;
 import lombok.extern.slf4j.Slf4j;
@@ -29,9 +30,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         QueryWrapper<User> queryWrapper = new QueryWrapper<User>()
                 .eq("uid", username);
         User user = userDao.selectOne(queryWrapper);
+        if (user == null) {
+            throw new UsernameNotFoundException(ResponseStatus.ACCOUNT_ERROR.toString());//账号错误
+        }
         log.debug("用户信息查询成功！，信息为：{}", user);
         user.setPassword("{bcrypt}" + new BCryptPasswordEncoder().encode(user.getPassword()));
-        //user.setPassword(new BCryptPasswordEncoder().encode("123456"));
         List<String> roles = userRoleDao.findUserRole(username);
         LoginUser loginUser = new LoginUser(user, roles);
         log.debug("用户权限信息为：{}", JSON.toJSONString(loginUser));
