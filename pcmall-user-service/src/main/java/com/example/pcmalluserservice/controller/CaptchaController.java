0e302b2f8f021a6e1b94375ba92a8829e7644408
Teriri <1511736399@qq.com>
2024-02-01 20:21:33 +0800

    CaptchaController

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/CaptchaController.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/CaptchaController.java
@@ -2,6 +2,7 @@ package com.example.pcmalluserservice.controller;
 
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.LineCaptcha;
+import com.example.pcmallcommon.response.ResponseResult;
 import com.example.pcmallcommon.utils.ImageUtils;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
@@ -18,13 +19,13 @@ public class CaptchaController {
     private HttpSession session;
 
     @RequestMapping("/api/captcha.jpg")
-    public String getCaptcha() {
+    public ResponseResult<String> getCaptcha() {
         LineCaptcha captcha = CaptchaUtil.createLineCaptcha(120, 40, 5, 30);
         String code = captcha.getCode();//获取验证码字符串
         session.setAttribute("captchaCode", code);//验证码字符串放入到session
         log.debug("验证码是：{}", code);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         captcha.write(out);
-        return ImageUtils.encodeImageString(out.toByteArray());
+        return ResponseResult.ok(ImageUtils.encodeImageString(out.toByteArray()));
     }
 }
