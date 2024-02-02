e59144660abd7c076884a9ecf78043130ebabb32
Teriri <1511736399@qq.com>
2024-02-02 19:57:07 +0800

    全局异常处理（WebFlux）

---
+++ pcmall-gateway/src/main/java/com/example/pcmallgateway/handler/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.example.pcmallgateway.handler;
+
+import com.alibaba.fastjson2.JSON;
+import com.example.pcmallcommon.exception.SystemException;
+import com.example.pcmallcommon.response.ResponseResult;
+import com.example.pcmallcommon.response.ResponseStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import reactor.netty.ByteBufMono;
+import reactor.util.annotation.NonNullApi;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
+    public GlobalExceptionHandler() {
+        log.debug("创建全局异常处理对象：GlobalExceptionHandler");
+    }
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        ResponseResult<String> message = null;
+        if (ex instanceof SystemException exception) {
+            log.error("发生自定义SystemException异常：{}", exception.getResponseStatus());
+            message = ResponseResult.error(exception.getResponseStatus());
+        } else {
+            log.error("发生Throwable异常：{}", ex.toString());
+            log.error("Class：{}", ex.getClass());
+            message = ResponseResult.error(ResponseStatus.INTERNAL_SERVER_ERROR);
+        }
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.BAD_REQUEST);
+        DataBuffer dataBuffer = response.bufferFactory().wrap(JSON.toJSONBytes(message));
+        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        return response.writeAndFlushWith(Mono.just(ByteBufMono.just(dataBuffer)));
+    }
+}
