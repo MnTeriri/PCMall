957f34d408cd13320602919e9645ceb68db1d40e
Teriri <1511736399@qq.com>
2024-02-02 19:56:51 +0800

    自定义异常

---
+++ pcmall-common/src/main/java/com/example/pcmallcommon/exception/SystemException.java
@@ -0,0 +1,17 @@
+package com.example.pcmallcommon.exception;
+
+import com.example.pcmallcommon.response.ResponseStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SystemException extends RuntimeException {
+    private ResponseStatus responseStatus;
+
+    public SystemException(ResponseStatus responseStatus) {
+        super(responseStatus.toString());
+        this.responseStatus = responseStatus;
+    }
+
+}
