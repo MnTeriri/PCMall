9e09e38c28741294101ab2df8a375d6773264cc3
Teriri <1511736399@qq.com>
2024-02-02 19:59:41 +0800

    IUserService

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/service/IUserService.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/service/IUserService.java
@@ -4,10 +4,8 @@ import com.example.pcmallcommon.model.User;
 import com.example.pcmallcommon.response.ResponseResult;
 
 public interface IUserService {
-//    public String login(String uid, String password);
-
     public ResponseResult<User> login(String uid, String password);
 
-    public String register();
+    public ResponseResult<String> register(String uid, String password);
 
 }
