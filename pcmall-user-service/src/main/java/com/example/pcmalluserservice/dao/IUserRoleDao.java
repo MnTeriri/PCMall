e7bb43f8c65be77fe70ca2c50ca4d8ad5612e4d9
Teriri <1511736399@qq.com>
2024-02-02 19:59:33 +0800

    IUserRoleDao

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/dao/IUserRoleDao.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/dao/IUserRoleDao.java
@@ -1,5 +1,6 @@
 package com.example.pcmalluserservice.dao;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,8 @@ public interface IUserRoleDao {
     @Select("SELECT role.name FROM role INNER JOIN user_role " +
             "ON role.id = user_role.rid " +
             "WHERE user_role.uid=#{uid}")
-    List<String> findUserRole(String uid);
+    public List<String> findUserRole(String uid);
+
+    @Insert("INSERT INTO user_role(uid, rid) VALUE (#{uid},#{rid})")
+    public Integer insertUserRole(String uid, Integer rid);
 }
