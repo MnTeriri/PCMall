5ad4c0ce6892fe8879a70c5c4f9bc7c0632cb5b7
Teriri <1511736399@qq.com>
2024-02-02 19:57:29 +0800

    user实体类

--- pcmall-common/src/main/java/com/example/pcmallcommon/model/User.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/model/User.java
@@ -1,9 +1,13 @@
 package com.example.pcmallcommon.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,8 +15,10 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Accessors(chain = true)
 @TableName("user")
 public class User {
+    @TableId(type = IdType.AUTO)
     private Integer id;
     private String uid;//用户号
     private String uname;//用户名
