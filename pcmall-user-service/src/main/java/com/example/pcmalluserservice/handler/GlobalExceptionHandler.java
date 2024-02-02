14a87d73d74ec9bb38a182213e7bcee2cb0ff1db
Teriri <1511736399@qq.com>
2024-02-02 19:58:12 +0800

    全局异常处理（WebMVC）

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/handler/GlobalExceptionHandler.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/handler/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package com.example.pcmalluserservice.handler;
 
+import com.example.pcmallcommon.exception.SystemException;
 import com.example.pcmallcommon.response.ResponseResult;
 import com.example.pcmallcommon.response.ResponseStatus;
-import com.example.pcmalluserservice.exception.SystemException;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,6 +20,7 @@ public class GlobalExceptionHandler {
 
     //处理自定义异常
     @ExceptionHandler(SystemException.class)
+    @org.springframework.web.bind.annotation.ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseResult<String> handlerSystemException(SystemException exception) {
         log.error("发生自定义SystemException异常：{}", exception.getResponseStatus());
         return ResponseResult.error(exception.getResponseStatus());
@@ -28,6 +28,7 @@ public class GlobalExceptionHandler {
 
     //处理账号和密码错误异常
     @ExceptionHandler(BadCredentialsException.class)
+    @org.springframework.web.bind.annotation.ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseResult<String> handlerBadCredentialsException(BadCredentialsException exception) {
         log.error("发生BadCredentialsException异常：{}", exception.getMessage());
         return ResponseResult.error(ResponseStatus.ACCOUNT_ERROR);
@@ -35,22 +36,33 @@ public class GlobalExceptionHandler {
 
     //处理权限异常
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseResult<String> handlerAccessDeniedException(HttpServletResponse response, AccessDeniedException e) {
+    @org.springframework.web.bind.annotation.ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseResult<String> handlerAccessDeniedException(AccessDeniedException e) {
         log.error("发生AccessDeniedException异常：{}", e.getMessage());
         log.error("Class：{}", e.getClass());
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         ResponseResult<String> message = ResponseResult.error(ResponseStatus.FORBIDDEN_ERROR);
         log.error(message.toString());
         return message;
     }
 
-    //处理Authentication内部其他异常
+    //处理AuthenticationException内部其他异常
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseResult<String> handlerAuthenticationException(HttpServletResponse response, AuthenticationException e) {
+    @org.springframework.web.bind.annotation.ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseResult<String> handlerAuthenticationException(AuthenticationException e) {
         log.error("发生AuthenticationException异常：{}", e.getMessage());
         log.error("Class：{}", e.getClass());
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         ResponseResult<String> message = ResponseResult.error(ResponseStatus.AUTHORIZED_ERROR);
+        log.error(message.toString());
+        return message;
+    }
+
+    //处理Exception异常
+    @ExceptionHandler(Exception.class)
+    @org.springframework.web.bind.annotation.ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseResult<String> handlerException(Exception e) {
+        log.error("发生Exception异常：{}", e.getMessage());
+        log.error("Class：{}", e.getClass());
+        ResponseResult<String> message = ResponseResult.error(ResponseStatus.INTERNAL_SERVER_ERROR);
         log.error(message.toString());
         return message;
     }
