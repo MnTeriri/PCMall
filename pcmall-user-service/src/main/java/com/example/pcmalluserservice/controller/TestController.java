6792ecbf3f25998ff1e13f8e37f3efa19b085102
Teriri <1511736399@qq.com>
2024-02-06 17:02:33 +0800

    TestController

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/TestController.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @RequestMapping("/api/test")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('TTTTT')")
     public String test() {
         return "akshfkjahsfjasf";
     }
