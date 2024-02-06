ffa42a84bf2ac01bf106fadeeee7f1cca43aeb33
Teriri <1511736399@qq.com>
2024-02-06 17:02:15 +0800

    LoginController

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/LoginController.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/LoginController.java
@@ -1,6 +1,6 @@
 package com.example.pcmalluserservice.controller;
 
-import com.alibaba.fastjson2.JSON;
+import com.example.pcmallcommon.exception.SystemException;
 import com.example.pcmallcommon.model.User;
 import com.example.pcmallcommon.response.ResponseResult;
 import com.example.pcmallcommon.response.ResponseStatus;
@@ -8,10 +8,8 @@ import com.example.pcmalluserservice.service.IUserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -25,10 +23,10 @@ public class LoginController {
     @PostMapping(value = "/api/login")
     public ResponseResult<User> login(String uid, String password, String code) {
         //判定验证码
-//        String captchaCode = (String) session.getAttribute("captchaCode");
-//        if (!captchaCode.equals(code)) {
-//            return ResponseResult.error(ResponseStatus.CAPTCHA_ERROR);
-//        }
+        String captchaCode = (String) session.getAttribute("captchaCode");
+        if (!captchaCode.equals(code)) {
+            throw new SystemException(ResponseStatus.CAPTCHA_ERROR);
+        }
         return userService.login(uid, password);
     }
 }
