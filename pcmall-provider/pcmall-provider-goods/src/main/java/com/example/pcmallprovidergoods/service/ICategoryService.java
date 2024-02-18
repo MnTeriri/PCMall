bd6f94498ca68b53433bd809bf0d7647a6039d10
Teriri <1511736399@qq.com>
2024-02-18 22:28:04 +0800

    CategoryService

--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/service/ICategoryService.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/service/ICategoryService.java
@@ -1,4 +1,16 @@
 package com.example.pcmallprovidergoods.service;
 
+import com.example.pcmallcommon.model.Category;
+
+import java.util.List;
+
 public interface ICategoryService {
+    public List<Category> getCategoryList(Integer currentPage, Integer pageSize);
+
+    public Long getTotalCount();
+
+    public Integer addCategory(Category category);
+
+    public Integer updateCategory(Category category);
+//    public List<Category> getNotDeleteCategoryList();
 }
--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/service/impl/CategoryServiceImpl.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/service/impl/CategoryServiceImpl.java
@@ -1,10 +1,15 @@
 package com.example.pcmallprovidergoods.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.pcmallcommon.model.Category;
 import com.example.pcmallprovidergoods.dao.ICategoryDao;
 import com.example.pcmallprovidergoods.service.ICategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -14,5 +19,27 @@ public class CategoryServiceImpl implements ICategoryService {
 
     public CategoryServiceImpl() {
         log.debug("创建Service对象：CategoryServiceImpl");
+    }
+
+    @Override
+    public List<Category> getCategoryList(Integer currentPage, Integer pageSize) {
+        Page<Category> page = new Page<>(currentPage, pageSize);
+        return categoryDao.selectPage(page,null).getRecords();
+    }
+
+    @Override
+    public Long getTotalCount() {
+        return categoryDao.selectCount(null);
+    }
+
+    @Override
+    public Integer addCategory(Category category) {
+        return categoryDao.insert(category);
+    }
+
+    @Override
+    public Integer updateCategory(Category category) {
+        category.setUpdateTime(LocalDateTime.now());
+        return categoryDao.updateById(category);
     }
 }
