a43857761f83cf47358feca5b3221defea73e601
Teriri <1511736399@qq.com>
2024-02-18 22:25:05 +0800

    跨域配置（关闭）

--- pcmall-consumer/pcmall-consumer-admin/src/main/java/com/example/pcmallconsumeradmin/config/CorsConfig.java
+++ pcmall-consumer/pcmall-consumer-admin/src/main/java/com/example/pcmallconsumeradmin/config/CorsConfig.java
@@ -1,4 +1,4 @@
-//package com.example.pcmalluserservice.config;
+//package com.example.pcmallconsumeradmin.config;
 //
 //import lombok.extern.slf4j.Slf4j;
 //import org.springframework.context.annotation.Configuration;
