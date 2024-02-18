ad9b04798278ca247912f38b590d511bd4acfa82
Teriri <1511736399@qq.com>
2024-02-18 22:30:27 +0800

    gateway filter

--- pcmall-gateway/src/main/java/com/example/pcmallgateway/filter/AuthenticationFilter.java
+++ pcmall-gateway/src/main/java/com/example/pcmallgateway/filter/AuthenticationFilter.java
@@ -1,17 +1,12 @@
 package com.example.pcmallgateway.filter;
 
 import com.example.pcmallcommon.exception.SystemException;
-import com.example.pcmallcommon.response.ResponseResult;
-import com.example.pcmallcommon.response.ResponseStatus;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.pcmallcommon.response.ResponseCode;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -24,7 +19,6 @@ import java.util.List;
 @Slf4j
 @Component
 public class AuthenticationFilter implements GlobalFilter, Ordered {
-    @SneakyThrows
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.debug("进入AuthenticationFilter");
@@ -32,6 +26,7 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
         ServerHttpResponse response = exchange.getResponse();
         response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
         String path = request.getURI().getPath();
+        log.debug("访问的链接是：{}", path);
         if ("/api/login".equals(path)
                 || "/api/register".equals(path)
                 || "/api/captcha.jpg".equals(path)
@@ -41,15 +36,8 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
         }
         List<String> token = request.getHeaders().get("token");
         if (token == null) {
-//            ResponseResult<String> message = ResponseResult.error(ResponseStatus.NO_TOKEN_ERROR);
-//            log.error(message.toString());
-//            response.setStatusCode(HttpStatus.FORBIDDEN);
-//            DataBufferFactory bufferFactory = response.bufferFactory();
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            DataBuffer wrap = bufferFactory.wrap(objectMapper.writeValueAsBytes(message));
-//            return response.writeWith(Mono.fromSupplier(() -> wrap));
-            log.error(ResponseStatus.NO_TOKEN_ERROR.toString());
-            throw new SystemException(ResponseStatus.NO_TOKEN_ERROR);
+            log.error(ResponseCode.NO_TOKEN_ERROR.toString());
+            throw new SystemException(ResponseCode.NO_TOKEN_ERROR);
         }
         log.debug("token:{}", request.getHeaders().get("token"));
         return chain.filter(exchange);
@@ -57,6 +45,6 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
 
     @Override
     public int getOrder() {
-        return Ordered.HIGHEST_PRECEDENCE;
+        return HIGHEST_PRECEDENCE;
     }
 }
