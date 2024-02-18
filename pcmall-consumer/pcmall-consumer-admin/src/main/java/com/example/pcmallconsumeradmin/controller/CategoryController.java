df2456846cfc646597a3aa0d24d59d2159edf602
Teriri <1511736399@qq.com>
2024-02-18 22:27:48 +0800

    CategoryController

--- pcmall-consumer/pcmall-consumer-admin/src/main/java/com/example/pcmallconsumeradmin/controller/CategoryController.java
+++ pcmall-consumer/pcmall-consumer-admin/src/main/java/com/example/pcmallconsumeradmin/controller/CategoryController.java
@@ -1,16 +1,56 @@
 package com.example.pcmallconsumeradmin.controller;
 
+import com.example.pcmallcommon.model.Category;
+import com.example.pcmallcommon.response.ResponseResult;
+import com.example.pcmallconsumeradmin.client.CategoryClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/category")
 @PreAuthorize("hasRole('ADMIN')")
 public class CategoryController {
+    @Autowired
+    private CategoryClient categoryClient;
+
     public CategoryController() {
         log.debug("创建Controller对象：CategoryController");
+    }
+
+    @PostMapping("/getCategoryList")
+    public ResponseResult<List<Category>> getCategoryList(
+            @RequestParam(defaultValue = "1") Integer currentPage,
+            @RequestParam(defaultValue = "10") Integer pageSize) {
+        return categoryClient.getCategoryList(currentPage, pageSize);
+    }
+
+    @PostMapping("/getTotalCount")
+    public ResponseResult<Long> getTotalCount(){
+        return categoryClient.getTotalCount();
+    }
+
+    @PostMapping("/addCategory")
+    public ResponseResult<String> addCategory(Category category){
+        return categoryClient.addCategory(category);
+    }
+
+    @PostMapping("/updateCategory")
+    public ResponseResult<String> updateCategory(Category category) {
+        return categoryClient.updateCategory(category);
+    }
+
+    @PostMapping("/deleteCategory")
+    public ResponseResult<String> deleteCategory(Category category){
+        return categoryClient.deleteCategory(category);
+    }
+
+    @PostMapping("/recoverCategory")
+    public ResponseResult<String> recoverCategory(Category category){
+        return categoryClient.recoverCategory(category);
     }
 }
--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/controller/CategoryController.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/controller/CategoryController.java
@@ -1,10 +1,14 @@
 package com.example.pcmallprovidergoods.controller;
 
+import com.example.pcmallcommon.model.Category;
+import com.example.pcmallcommon.response.ResponseResult;
 import com.example.pcmallprovidergoods.service.ICategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -15,5 +19,51 @@ public class CategoryController {
 
     public CategoryController() {
         log.debug("创建Controller对象：CategoryController");
+    }
+
+    @PostMapping("/getCategoryList")
+    public ResponseResult<List<Category>> getCategoryList(Integer currentPage, Integer pageSize) {
+        List<Category> categoryList = categoryService.getCategoryList(currentPage,pageSize);
+        return ResponseResult.ok(categoryList);
+    }
+
+    @PostMapping("/getTotalCount")
+    public ResponseResult<Long> getTotalCount(){
+        Long count = categoryService.getTotalCount();
+        return ResponseResult.ok(count);
+    }
+
+    @PostMapping(value = "/addCategory")
+    public ResponseResult<String> addCategory(@RequestBody Category category){
+        if (categoryService.addCategory(category) == 1) {
+            return ResponseResult.ok("添加成功");
+        }
+        return ResponseResult.error("添加失败");
+    }
+
+    @PostMapping("/updateCategory")
+    public ResponseResult<String> updateCategory(@RequestBody Category category){
+        if (categoryService.updateCategory(category) == 1) {
+            return ResponseResult.ok("修改成功");
+        }
+        return ResponseResult.error("修改失败");
+    }
+
+    @PostMapping("/deleteCategory")
+    public ResponseResult<String> deleteCategory(@RequestBody Category category){
+        category.setIsDelete(1);
+        if (categoryService.updateCategory(category) == 1) {
+            return ResponseResult.ok("删除成功");
+        }
+        return ResponseResult.error("删除失败");
+    }
+
+    @PostMapping("/recoverCategory")
+    public ResponseResult<String> recoverCategory(@RequestBody Category category){
+        category.setIsDelete(0);
+        if (categoryService.updateCategory(category) == 1) {
+            return ResponseResult.ok("恢复成功");
+        }
+        return ResponseResult.error("恢复失败");
     }
 }
