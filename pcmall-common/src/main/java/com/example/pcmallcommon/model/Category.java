123428148249c0c1aceb5a466b95616dbe8f5ce7
Teriri <1511736399@qq.com>
2024-02-18 22:27:32 +0800

    实体类

--- pcmall-common/src/main/java/com/example/pcmallcommon/model/Category.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/model/Category.java
@@ -3,6 +3,7 @@ package com.example.pcmallcommon.model;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +20,9 @@ public class Category {
     @TableId(type = IdType.AUTO)
     private Integer id;//分类编号
     private String cname;//分类名称
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
     private LocalDateTime createdTime;//创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
     private LocalDateTime updateTime;//修改时间
     private Integer isDelete;//是否删除（0正常 1删除）
 }
