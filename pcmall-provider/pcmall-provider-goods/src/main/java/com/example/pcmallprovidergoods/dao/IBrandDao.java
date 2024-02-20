665fd1d9d2b763e87c87279ef212412d3ace1943
Teriri <1511736399@qq.com>
2024-02-20 18:17:42 +0800

    BrandDao

--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/IBrandDao.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/IBrandDao.java
@@ -2,8 +2,24 @@ package com.example.pcmallprovidergoods.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.pcmallcommon.model.Brand;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IBrandDao extends BaseMapper<Brand> {
+    @Select("SELECT COUNT(id) FROM category_brand WHERE bid=#{bid};")
+    public Long getCategoryCount(Integer bid);
+
+    @Select("SELECT cid FROM category_brand WHERE bid=#{bid};")
+    public List<Integer> getSelectedCategoryIdList(Integer bid);
+
+    @Insert("INSERT INTO category_brand(cid, bid) VALUE (#{cid},#{bid});")
+    public Integer insertCategoryBrand(Integer bid, Integer cid);
+
+    @Delete("DELETE FROM category_brand WHERE cid=#{cid} AND bid=#{bid};")
+    public Integer deleteCategoryBrand(Integer bid, Integer cid);
 }
