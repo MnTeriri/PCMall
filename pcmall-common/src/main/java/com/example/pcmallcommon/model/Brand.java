ecf26813e49cab6848a5d7d10c181601f9c505bf
Teriri <1511736399@qq.com>
2024-02-20 18:16:53 +0800

    实体类

--- pcmall-common/src/main/java/com/example/pcmallcommon/model/Brand.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/model/Brand.java
@@ -1,8 +1,10 @@
 package com.example.pcmallcommon.model;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +21,12 @@ public class Brand {
     @TableId(type = IdType.AUTO)
     private Integer id;//品牌id
     private String bname;//品牌名称
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createdTime;//创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime updateTime;//修改时间
+    @TableField(exist = false)
+    private Long categoryCount;
+    private String image;
     private Integer isDelete;//是否删除（0正常 1删除）
 }
--- pcmall-common/src/main/java/com/example/pcmallcommon/model/User.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/model/User.java
@@ -25,7 +25,7 @@ public class User {
     private String password;//密码（MD5加密）
     private LocalDateTime createdTime;//创建时间
     private LocalDateTime loginTime;//最后一次登录时间
-    // private byte[] image;
+    private String image;
     private Integer isDelete;//是否删除（0正常 1删除）
     //private String imageString;//头像
 }
