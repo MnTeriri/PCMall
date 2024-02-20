6e65dd29f7b7430c04db071299d2c7dc2f0d8f2a
Teriri <1511736399@qq.com>
2024-02-20 18:17:16 +0800

    BrandController

--- pcmall-consumer/pcmall-consumer-admin/src/main/java/com/example/pcmallconsumeradmin/controller/BrandController.java
+++ pcmall-consumer/pcmall-consumer-admin/src/main/java/com/example/pcmallconsumeradmin/controller/BrandController.java
@@ -1,17 +1,69 @@
 package com.example.pcmallconsumeradmin.controller;
 
+import com.example.pcmallcommon.model.Brand;
+import com.example.pcmallcommon.response.ResponseResult;
+import com.example.pcmallconsumeradmin.client.BrandClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/brand")
 @PreAuthorize("hasRole('ADMIN')")
 public class BrandController {
+    @Autowired
+    private BrandClient brandClient;
 
     public BrandController() {
         log.debug("创建Controller对象：BrandController");
+    }
+
+    @PostMapping("/getBrandList")
+    public ResponseResult<List<Brand>> getBrandList(
+            @RequestParam(defaultValue = "1") Integer currentPage,
+            @RequestParam(defaultValue = "10") Integer pageSize) {
+        return brandClient.getBrandList(currentPage, pageSize);
+    }
+
+    @PostMapping("/getSelectedCategoryIdList")
+    public ResponseResult<List<Integer>> getSelectedCategoryIdList(Integer bid) {
+        return brandClient.getSelectedCategoryIdList(bid);
+    }
+
+    @PostMapping("/getTotalCount")
+    public ResponseResult<Long> getTotalCount(){
+        return brandClient.getTotalCount();
+    }
+
+    @PostMapping("/addBrand")
+    public ResponseResult<String> addBrand(Brand brand) {
+        return brandClient.addBrand(brand);
+    }
+
+    @PostMapping("/updateBrand")
+    public ResponseResult<String> updateBrand(Brand brand) {
+        return brandClient.updateBrand(brand);
+    }
+
+    @PostMapping("/deleteBrand")
+    public ResponseResult<String> deleteBrand(Brand brand) {
+        return brandClient.deleteBrand(brand);
+    }
+
+    @PostMapping("/recoverBrand")
+    public ResponseResult<String> recoverBrand(Brand brand) {
+        return brandClient.recoverBrand(brand);
+    }
+
+    @PostMapping("/brandCategoryChange")
+    public ResponseResult<String> brandCategoryChange(Integer bid, Integer cid, Boolean selected) {
+        return brandClient.brandCategoryChange(bid, cid, selected);
     }
 }
--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/controller/BrandController.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/controller/BrandController.java
@@ -1,10 +1,16 @@
 package com.example.pcmallprovidergoods.controller;
 
+import com.example.pcmallcommon.model.Brand;
+import com.example.pcmallcommon.response.ResponseResult;
 import com.example.pcmallprovidergoods.service.IBrandService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -15,5 +21,65 @@ public class BrandController {
 
     public BrandController() {
         log.debug("创建Controller对象：BrandController");
+    }
+
+    @PostMapping("/getBrandList")
+    public ResponseResult<List<Brand>> getBrandList(Integer currentPage, Integer pageSize) {
+        List<Brand> brandList = brandService.getBrandList(currentPage, pageSize);
+        return ResponseResult.ok(brandList);
+    }
+
+    @PostMapping("/getSelectedCategoryIdList")
+    public ResponseResult<List<Integer>> getSelectedCategoryIdList(Integer bid) {
+        List<Integer> categoryList = brandService.getSelectedCategoryIdList(bid);
+        return ResponseResult.ok(categoryList);
+    }
+
+    @PostMapping("/getTotalCount")
+    public ResponseResult<Long> getTotalCount(){
+        Long count = brandService.getTotalCount();
+        return ResponseResult.ok(count);
+    }
+
+    @PostMapping("/addBrand")
+    public ResponseResult<String> addBrand(@RequestBody Brand brand) {
+        if (brandService.addBrand(brand) == 1) {
+            return ResponseResult.ok("添加成功");
+        }
+        return ResponseResult.error("添加失败");
+    }
+
+    @PostMapping("/updateBrand")
+    public ResponseResult<String> updateBrand(@RequestBody Brand brand) {
+        if (brandService.updateBrand(brand) == 1) {
+            return ResponseResult.ok("修改成功");
+        }
+        return ResponseResult.error("修改失败");
+    }
+
+    @PostMapping("/deleteBrand")
+    public ResponseResult<String> deleteBrand(@RequestBody Brand brand) {
+        brand.setIsDelete(1);
+        if (brandService.updateBrand(brand) == 1) {
+            return ResponseResult.ok("删除成功");
+        }
+        return ResponseResult.error("删除失败");
+    }
+
+    @PostMapping("/recoverBrand")
+    public ResponseResult<String> recoverBrand(@RequestBody Brand brand) {
+        brand.setIsDelete(0);
+        if (brandService.updateBrand(brand) == 1) {
+            return ResponseResult.ok("恢复成功");
+        }
+        return ResponseResult.error("恢复失败");
+    }
+
+    @PostMapping("/brandCategoryChange")
+    public ResponseResult<String> brandCategoryChange(Integer bid, Integer cid, Boolean selected) {
+        if (brandService.brandCategoryChange(bid, cid, selected) == 1) {
+            return ResponseResult.ok("修改成功");
+        }
+        return ResponseResult.error("修改失败");
     }
 }
