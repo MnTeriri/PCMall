4a986213ebd2be0eb8301e8e2bb59b56f64b30d0
Teriri <1511736399@qq.com>
2024-02-20 18:18:22 +0800

    BrandClient

--- pcmall-consumer/pcmall-consumer-admin/src/main/java/com/example/pcmallconsumeradmin/client/BrandClient.java
+++ pcmall-consumer/pcmall-consumer-admin/src/main/java/com/example/pcmallconsumeradmin/client/BrandClient.java
@@ -1,7 +1,42 @@
 package com.example.pcmallconsumeradmin.client;
 
+import com.example.pcmallcommon.model.Brand;
+import com.example.pcmallcommon.response.ResponseResult;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient(contextId = "brandClient", value = "pcmall-provider-goods")
 public interface BrandClient {
+    @PostMapping("/brand/getBrandList")
+    public ResponseResult<List<Brand>> getBrandList(
+            @RequestParam("currentPage") Integer currentPage,
+            @RequestParam("pageSize") Integer pageSize);
+
+    @PostMapping("/brand/getTotalCount")
+    public ResponseResult<Long> getTotalCount();
+
+    @PostMapping("/brand/getSelectedCategoryIdList")
+    public ResponseResult<List<Integer>> getSelectedCategoryIdList(@RequestParam("bid") Integer bid);
+
+    @PostMapping("/brand/addBrand")
+    public ResponseResult<String> addBrand(@RequestBody Brand brand);
+
+    @PostMapping("/brand/updateBrand")
+    public ResponseResult<String> updateBrand(@RequestBody Brand brand);
+
+    @PostMapping("/brand/deleteBrand")
+    public ResponseResult<String> deleteBrand(@RequestBody Brand brand);
+
+    @PostMapping("/brand/recoverBrand")
+    public ResponseResult<String> recoverBrand(@RequestBody Brand brand);
+
+    @PostMapping("/brand/brandCategoryChange")
+    public ResponseResult<String> brandCategoryChange(
+            @RequestParam("bid") Integer bid,
+            @RequestParam("cid") Integer cid,
+            @RequestParam("selected") Boolean selected);
 }
