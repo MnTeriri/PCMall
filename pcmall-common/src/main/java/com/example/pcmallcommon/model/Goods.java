adbdb32c4fffd3b33ac8506382566d583dcdb6ac
Teriri <1511736399@qq.com>
2024-02-21 19:04:05 +0800

    实体类

--- pcmall-common/src/main/java/com/example/pcmallcommon/model/Goods.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/model/Goods.java
@@ -1,8 +1,10 @@
 package com.example.pcmallcommon.model;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,9 +22,15 @@ public class Goods {
     @TableId(type = IdType.AUTO)
     private Integer id;//商品编号
     private Integer cid;//分类编号，参考category的主键
+    @TableField(exist = false)
+    private Category category;
     private Integer bid;//品牌编号，参考brand的主键
+    @TableField(exist = false)
+    private Brand brand;
     private String gname;//商品名称
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createdTime;//创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime updateTime;//修改时间
     private String image;//图片
     private BigDecimal price;//价格
