a6ace69a7a5c074debdd71f3f1bb9e08d029fe5a
Teriri <1511736399@qq.com>
2024-02-21 19:05:10 +0800

    CategoryDao

--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/ICategoryDao.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/ICategoryDao.java
@@ -2,8 +2,11 @@ package com.example.pcmallprovidergoods.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.pcmallcommon.model.Category;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICategoryDao extends BaseMapper<Category> {
+    @Select("SELECT * FROM category WHERE id=#{id}")
+    public Category selectCategoryById(Integer id);
 }
