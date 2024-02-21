6b3a42f36012e31e26a48b2b50a29090031e32c7
Teriri <1511736399@qq.com>
2024-02-21 19:04:53 +0800

    GoodsService

--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/service/IGoodsService.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/service/IGoodsService.java
@@ -1,4 +1,15 @@
 package com.example.pcmallprovidergoods.service;
 
+import com.example.pcmallcommon.model.Goods;
+
+import java.util.List;
+
 public interface IGoodsService {
+    public List<Goods> getGoodsList(Integer currentPage, Integer pageSize);
+
+    public Long getTotalCount();
+
+    public Integer addGoods(Goods goods);
+
+    public Integer updateGoods(Goods goods);
 }
--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/service/impl/GoodsServiceImpl.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/service/impl/GoodsServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.pcmallprovidergoods.service.impl;
 
+import com.example.pcmallcommon.model.Goods;
 import com.example.pcmallprovidergoods.dao.IGoodsDao;
 import com.example.pcmallprovidergoods.service.IGoodsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -14,5 +18,26 @@ public class GoodsServiceImpl implements IGoodsService {
 
     public GoodsServiceImpl() {
         log.debug("创建Service对象：GoodsServiceImpl");
+    }
+
+    @Override
+    public List<Goods> getGoodsList(Integer currentPage, Integer pageSize) {
+        return goodsDao.getGoodsList((currentPage - 1) * pageSize, pageSize);
+    }
+
+    @Override
+    public Long getTotalCount() {
+        return goodsDao.selectCount(null);
+    }
+
+    @Override
+    public Integer addGoods(Goods goods) {
+        return goodsDao.insert(goods);
+    }
+
+    @Override
+    public Integer updateGoods(Goods goods) {
+        goods.setUpdateTime(LocalDateTime.now());
+        return goodsDao.updateById(goods);
     }
 }
