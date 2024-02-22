40e9e0342f64c30a6c0e0c1b3554c6bd492a8573
Teriri <1511736399@qq.com>
2024-02-22 19:43:24 +0800

    Dao

--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/IGoodsDao.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/IGoodsDao.java
@@ -13,12 +13,21 @@ import java.util.List;
 
 @Repository
 public interface IGoodsDao extends BaseMapper<Goods> {
-    @Select("SELECT * FROM goods LIMIT #{start},#{pageSize}")
+    @Select("SELECT * FROM goods LIMIT #{start},#{pageSize};")
     @Results({
             @Result(property = "cid", column = "cid", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
             @Result(property = "bid", column = "bid", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-            @Result(property = "category",column = "cid",one = @One(select = "com.example.pcmallprovidergoods.dao.ICategoryDao.getCategoryById")),
-            @Result(property = "brand",column = "bid",one = @One(select = "com.example.pcmallprovidergoods.dao.IBrandDao.getBrandById"))
+            @Result(property = "category", column = "cid", one = @One(select = "com.example.pcmallprovidergoods.dao.ICategoryDao.getCategoryById")),
+            @Result(property = "brand", column = "bid", one = @One(select = "com.example.pcmallprovidergoods.dao.IBrandDao.getBrandById"))
     })
     public List<Goods> getGoodsList(Integer start, Integer pageSize);
+
+    @Select("SELECT * FROM goods WHERE id=#{id};")
+    @Results({
+            @Result(property = "cid", column = "cid", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+            @Result(property = "bid", column = "bid", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+            @Result(property = "category", column = "cid", one = @One(select = "com.example.pcmallprovidergoods.dao.ICategoryDao.getCategoryById")),
+            @Result(property = "brand", column = "bid", one = @One(select = "com.example.pcmallprovidergoods.dao.IBrandDao.getBrandById"))
+    })
+    public Goods searchGoodsById(Integer id);
 }
--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/IStorageDao.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/IStorageDao.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IStorageDao extends BaseMapper<Storage> {
+
 }
