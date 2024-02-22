4614e795b6c4051c57ffecdea2be729c2ad9facb
Teriri <1511736399@qq.com>
2024-02-22 19:42:41 +0800

    实体类

--- pcmall-common/src/main/java/com/example/pcmallcommon/model/Storage.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/model/Storage.java
@@ -3,6 +3,7 @@ package com.example.pcmallcommon.model;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,5 +23,6 @@ public class Storage {
     private String uid;//用户编号
     private Integer count;//数量
     private Integer status;//状态 0入库、1卖出、2退货、3取消订单
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createdTime;//创建时间
 }
