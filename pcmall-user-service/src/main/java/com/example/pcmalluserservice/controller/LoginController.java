24ba362a1cd0acce2ce69c8ef2d616b33be87ee7
Teriri <1511736399@qq.com>
2024-03-21 13:41:47 +0800

    Controller

--- pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/LoginController.java
+++ pcmall-user-service/src/main/java/com/example/pcmalluserservice/controller/LoginController.java
@@ -1,8 +1,6 @@
 package com.example.pcmalluserservice.controller;
 
-import com.example.pcmallcommon.exception.SystemException;
 import com.example.pcmallcommon.model.User;
-import com.example.pcmallcommon.response.ResponseCode;
 import com.example.pcmallcommon.response.ResponseResult;
 import com.example.pcmalluserservice.service.IUserService;
 import jakarta.servlet.http.HttpSession;
