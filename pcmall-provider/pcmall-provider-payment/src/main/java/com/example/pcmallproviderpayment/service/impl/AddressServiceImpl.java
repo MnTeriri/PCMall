d00deaa4cba88c4e8a7887d4a17777c2be0fbbb9
Teriri <1511736399@qq.com>
2024-03-21 13:40:33 +0800

    Service

--- pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/service/impl/AddressServiceImpl.java
+++ pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/service/impl/AddressServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.pcmallproviderpayment.service.impl;
 
+import com.example.pcmallcommon.model.Address;
 import com.example.pcmallproviderpayment.dao.IAddressDao;
 import com.example.pcmallproviderpayment.service.IAddressService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -14,5 +17,35 @@ public class AddressServiceImpl implements IAddressService {
 
     public AddressServiceImpl() {
         log.debug("创建Service对象：AddressServiceImpl");
+    }
+
+    @Override
+    public Address searchDefaultAddress(String uid) {
+        return null;
+    }
+
+    @Override
+    public List<Address> searchAddressByUid(String uid) {
+        return null;
+    }
+
+    @Override
+    public void addAddress(Address address) {
+
+    }
+
+    @Override
+    public void updateAddress(Address address) {
+
+    }
+
+    @Override
+    public void updateAddressByUid(Address address) {
+
+    }
+
+    @Override
+    public void deleteAddress(Address address) {
+
     }
 }
