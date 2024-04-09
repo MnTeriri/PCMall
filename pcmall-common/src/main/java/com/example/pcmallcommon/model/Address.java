67ffb7cdde315c3ad8fc477477faf4c029a0bb21
Teriri <1511736399@qq.com>
2024-04-09 19:23:01 +0800

    实体类

--- pcmall-common/src/main/java/com/example/pcmallcommon/model/Address.java
+++ pcmall-common/src/main/java/com/example/pcmallcommon/model/Address.java
@@ -3,6 +3,7 @@ package com.example.pcmallcommon.model;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,8 +26,10 @@ public class Address {
     private String address;//详细地址
     private String receiverName;//收件人
     private String phone;//手机号码
-    private LocalDateTime createTime;//创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createdTime;//创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime updateTime;//修改时间
     private Integer isDefault;//是否选中（0不选中 1选中）
-    private Integer isDelete;//是否删除（）
+    private Integer isDelete;//是否删除（0正常 1删除）
 }
