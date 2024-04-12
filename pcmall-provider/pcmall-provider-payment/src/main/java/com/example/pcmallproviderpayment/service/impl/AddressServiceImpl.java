364461534700933a04998c074659436e391a49b8
Teriri <1511736399@qq.com>
2024-04-12 19:25:00 +0800

    Service

--- pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/service/impl/AddressServiceImpl.java
+++ pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/service/impl/AddressServiceImpl.java
@@ -1,13 +1,17 @@
 package com.example.pcmallproviderpayment.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.pcmallcommon.exception.SystemException;
 import com.example.pcmallcommon.model.Address;
+import com.example.pcmallcommon.response.ResponseCode;
 import com.example.pcmallproviderpayment.dao.IAddressDao;
 import com.example.pcmallproviderpayment.service.IAddressService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -24,7 +28,9 @@ public class AddressServiceImpl implements IAddressService {
     public List<Address> searchAddressList(String uid) {
         QueryWrapper<Address> queryWrapper = new QueryWrapper<Address>()
                 .eq("uid", uid)
-                .eq("is_delete", 0);
+                .eq("is_delete", 0)
+                .orderByDesc("is_default")
+                .orderByDesc("id");
         return addressDao.selectList(queryWrapper);
     }
 
@@ -33,15 +39,29 @@ public class AddressServiceImpl implements IAddressService {
         return null;
     }
 
-
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public void addAddress(Address address) {
-
+        //如果该地址被设置成默认
+        if (address.getIsDefault() == 1) {
+            addressDao.cleanUserDefaultAddress(address.getUid());//清除原默认地址
+        }
+        if (addressDao.insert(address) != 1) {
+            throw new SystemException(ResponseCode.ERROR);
+        }
     }
 
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public void updateAddress(Address address) {
-
+        address.setUpdateTime(LocalDateTime.now());
+        //如果该地址被设置成默认
+        if (address.getIsDefault() == 1) {
+            addressDao.cleanUserDefaultAddress(address.getUid());//清除原默认地址
+        }
+        if (addressDao.updateById(address) != 1) {
+            throw new SystemException(ResponseCode.ERROR);
+        }
     }
 
     @Override
