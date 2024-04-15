7f997fa8e809419233fa1375ed3929e1aab9b9fe
Teriri <1511736399@qq.com>
2024-04-15 19:24:43 +0800

    Application

--- pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/PcmallProviderPaymentApplication.java
+++ pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/PcmallProviderPaymentApplication.java
@@ -1,11 +1,13 @@
 package com.example.pcmallproviderpayment;
 
+import cn.hutool.extra.spring.EnableSpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSpringUtil
 public class PcmallProviderPaymentApplication {
 
 	public static void main(String[] args) {
