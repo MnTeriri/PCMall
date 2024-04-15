57708a8089047e2c72b4d5f4c6476c8c1acfd0a1
Teriri <1511736399@qq.com>
2024-04-15 19:25:18 +0800

    Service

--- pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/service/IOrderService.java
+++ pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/service/IOrderService.java
@@ -5,7 +5,12 @@ import com.example.pcmallcommon.model.Order;
 import java.util.List;
 
 public interface IOrderService {
-    public List<Order> searchOrderList();
+    public List<Order> searchOrderList(String searchValue, String uid, Integer type, Integer currentPage, Integer pageSize);
 
-    public Long getRecordsFiltered(String uid, Integer type);
+    public Long getRecordsFiltered(String searchValue, String uid, Integer type);
+
+    public void createOrder(String uid, Integer aid);
+
+    //status只有两个值（4交易取消、6退货完成）
+    public void cancelOrder(String oid, Integer status);
 }
--- pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/service/impl/OrderServiceImpl.java
+++ pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/service/impl/OrderServiceImpl.java
@@ -1,31 +1,102 @@
 package com.example.pcmallproviderpayment.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.pcmallcommon.exception.SystemException;
 import com.example.pcmallcommon.model.Order;
+import com.example.pcmallcommon.response.ResponseCode;
 import com.example.pcmallproviderpayment.dao.IOrderDao;
+import com.example.pcmallproviderpayment.job.OrderJob;
 import com.example.pcmallproviderpayment.service.IOrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class OrderServiceImpl implements IOrderService {
     @Autowired
     private IOrderDao orderDao;
+    @Autowired
+    private SchedulerFactoryBean schedulerFactoryBean;
 
     public OrderServiceImpl() {
         log.debug("创建Service对象：OrderServiceImpl");
     }
 
     @Override
-    public List<Order> searchOrderList() {
-        return null;
+    public List<Order> searchOrderList(String searchValue, String uid, Integer type, Integer currentPage, Integer pageSize) {
+        return orderDao.searchOrderList(searchValue, uid, type, (currentPage - 1) * pageSize, pageSize);
     }
 
     @Override
-    public Long getRecordsFiltered(String uid, Integer type) {
-        return orderDao.getRecordsFiltered(uid, type);
+    public Long getRecordsFiltered(String searchValue, String uid, Integer type) {
+        return orderDao.getRecordsFiltered(searchValue, uid, type);
+    }
+
+    @Override
+    public void createOrder(String uid, Integer aid) {
+        String oid = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + RandomUtil.randomNumbers(6);
+        while (orderDao.selectCount(new QueryWrapper<Order>().eq("oid", oid)) != 0) {//如果生成的订单号存在，则重新生成，直到不存在
+            oid = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + RandomUtil.randomNumbers(6);
+        }
+        Map<String, Object> data = new HashMap<>();
+        data.put("oid", oid);
+        data.put("uid", uid);
+        data.put("aid", aid);
+        orderDao.createOrder(data);//执行存储过程
+        Integer result = (Integer) data.get("result");//获取输出参数
+        if (result == -4) {
+            throw new SystemException(ResponseCode.ERROR);
+        } else if (result == -3) {
+            throw new SystemException(ResponseCode.CART_EMPTY_ERROR);
+        } else if (result == -2) {
+            throw new SystemException(ResponseCode.CART_GOODS_ERROR);
+        } else if (result == -1) {
+            throw new SystemException(ResponseCode.GOODS_NOT_ENOUGH_ERROR);
+        } else if (result == 1) {
+            //创建定时任务，15分钟自动关闭订单
+            JobDetail jobDetail = JobBuilder.newJob(OrderJob.class)
+                    .withIdentity(oid, "orderGroup")
+                    .usingJobData("orderOid", oid)
+                    .build();
+            LocalDateTime localDateTime = LocalDateTime.now().plusSeconds(10);
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity(oid, "orderGroup")
+                    .startAt(Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()))
+                    .build();
+            try {
+                Scheduler scheduler = schedulerFactoryBean.getScheduler();
+                scheduler.scheduleJob(jobDetail, trigger);//添加订单定时任务
+                log.debug("订单定时任务{}添加成功", scheduler);
+            } catch (SchedulerException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public void cancelOrder(String oid, Integer status) {
+        try {//删除对应订单的定时任务
+            Scheduler scheduler = schedulerFactoryBean.getScheduler();
+            scheduler.unscheduleJob(new TriggerKey(oid, "orderGroup"));
+        } catch (SchedulerException e) {
+            throw new RuntimeException(e);
+        }
+        Map<String, Object> data = new HashMap<>();
+        data.put("oid", oid);
+        data.put("status", status);
+        orderDao.cancelOrder(data);
+        System.out.println(data.get("result"));
     }
 }
