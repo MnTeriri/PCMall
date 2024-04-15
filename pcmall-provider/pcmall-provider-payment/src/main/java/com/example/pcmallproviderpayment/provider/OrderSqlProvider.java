558b44959d9889cb60da7ad4f5681b32d29f6a8c
Teriri <1511736399@qq.com>
2024-04-15 19:25:35 +0800

    OrderSqlProvider

--- pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/provider/OrderSqlProvider.java
+++ pcmall-provider/pcmall-provider-payment/src/main/java/com/example/pcmallproviderpayment/provider/OrderSqlProvider.java
@@ -3,10 +3,33 @@ package com.example.pcmallproviderpayment.provider;
 import org.apache.ibatis.annotations.Param;
 
 public class OrderSqlProvider {
-    public String getRecordsFilteredSql(@Param("uid") String uid, @Param("type") Integer type) {
-        String sql = "SELECT COUNT(*) FROM `order` WHERE uid=#{uid} ";
+    public String searchOrderListSql(
+            @Param("searchValue") String searchValue,
+            @Param("uid") String uid,
+            @Param("type") Integer type,
+            @Param("start") Integer start,
+            @Param("pageSize") Integer pageSize) {
+        String sql = "SELECT * FROM `order` WHERE oid LIKE CONCAT('%', #{searchValue}, '%')";
+        if (!"".equals(uid)) {//有uid则查对应uid，没有则全查
+            sql += " AND uid = #{uid}";
+        }
         if (type != -1) {//-1为查全部订单，其余的type数值对应status
-            sql += "AND status=#{type} ";
+            sql += " AND status = #{type}";
+        }
+        sql += " ORDER BY id DESC LIMIT #{start},#{pageSize};";
+        return sql;
+    }
+
+    public String getRecordsFilteredSql(
+            @Param("searchValue") String searchValue,
+            @Param("uid") String uid,
+            @Param("type") Integer type) {
+        String sql = "SELECT COUNT(*) FROM `order` WHERE oid LIKE CONCAT('%', #{searchValue}, '%')";
+        if (!"".equals(uid)) {//有uid则查对应uid，没有则全查
+            sql += " AND uid = #{uid}";
+        }
+        if (type != -1) {//-1为查全部订单，其余的type数值对应status
+            sql += " AND status = #{type}";
         }
         return sql;
     }
