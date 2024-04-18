2af74160f18573aa1f2053578c6c89448bc24bf4
Teriri <1511736399@qq.com>
2024-04-18 20:28:07 +0800

    Dao

--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/IGoodsDao.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/dao/IGoodsDao.java
@@ -35,6 +35,17 @@ public interface IGoodsDao extends BaseMapper<Goods> {
     })
     public List<Goods> searchGoodsList(String searchValue, Integer start, Integer pageSize);
 
+    @Select("SELECT * FROM goods " +
+            "WHERE cid=#{cid} AND bid=#{bid} AND status=0 AND is_delete=0 " +
+            "LIMIT #{start},#{pageSize};")
+    @Results({
+            @Result(property = "cid", column = "cid", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+            @Result(property = "bid", column = "bid", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
+            @Result(property = "category", column = "cid", one = @One(select = "com.example.pcmallprovidergoods.dao.ICategoryDao.searchCategory")),
+            @Result(property = "brand", column = "bid", one = @One(select = "com.example.pcmallprovidergoods.dao.IBrandDao.searchBrand"))
+    })
+    public List<Goods> searchGoodsByCidAndBid(Integer cid, Integer bid, Integer start, Integer pageSize);
+
     @Select("SELECT * FROM goods WHERE id=#{id};")
     @Results({
             @Result(property = "cid", column = "cid", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
