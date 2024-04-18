3f463ae328055da8d7073d838d531f7f85a84b2c
Teriri <1511736399@qq.com>
2024-04-18 20:28:55 +0800

    StorageCache

--- pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/cache/StorageCache.java
+++ pcmall-provider/pcmall-provider-goods/src/main/java/com/example/pcmallprovidergoods/cache/StorageCache.java
@@ -1,25 +1,29 @@
 package com.example.pcmallprovidergoods.cache;
 
 import cn.hutool.extra.spring.SpringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.cache.Cache;
 import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+@Slf4j
 public class StorageCache implements Cache {
     private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
     private final String id; // RedisCache实例Id
-    private final StringRedisTemplate stringRedisTemplate;
-    private static final long EXPIRE_TIME_IN_MINUTES = 5; // redis过期时间
+    private final RedisTemplate<String, Object> redisTemplate;
+    private static final long EXPIRE_TIME_IN_MINUTES = 30; // redis过期时间
 
     public StorageCache(String id) {
         if (id == null) {
             throw new IllegalArgumentException("Cache instances require an ID");
         }
-        stringRedisTemplate = SpringUtil.getBean("stringRedisTemplate");
+        redisTemplate = SpringUtil.getBean("redisTemplate");
         this.id = id;
+        log.debug("StorageCache对象创建");
     }
 
     @Override
@@ -28,22 +32,28 @@ public class StorageCache implements Cache {
     }
 
     @Override
-    public void putObject(Object o, Object o1) {
-
+    public void putObject(Object key, Object value) {
+        log.debug("put object key:{} value:{}", key.toString(), value);
+        redisTemplate.opsForValue().set(key.toString(), value);
+        redisTemplate.expire(key.toString(), EXPIRE_TIME_IN_MINUTES, TimeUnit.MINUTES);//设置过期时间
     }
 
     @Override
-    public Object getObject(Object o) {
-        return null;
+    public Object getObject(Object key) {
+        log.debug("get object key:{}", key.toString());
+        return redisTemplate.opsForValue().get(key.toString());
     }
 
     @Override
-    public Object removeObject(Object o) {
-        return null;
+    public Object removeObject(Object key) {
+        log.debug("remove object key:{}", key);
+        Object deleteObj = redisTemplate.opsForValue().get(key.toString());
+        redisTemplate.delete(key.toString());
+        return deleteObj;
     }
 
     public void clear() {
-        stringRedisTemplate.execute((RedisCallback<Object>) connection -> {
+        redisTemplate.execute((RedisCallback<Object>) connection -> {
             connection.flushDb();
             return null;
         });
