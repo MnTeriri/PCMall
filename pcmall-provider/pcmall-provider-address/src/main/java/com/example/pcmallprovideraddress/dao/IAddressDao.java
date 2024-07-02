ace3d1f9c4c27726f8540d04c9eaf7d398311fe8
Teriri <1511736399@qq.com>
2024-07-02 16:24:03 +0800

    Dao

---
+++ pcmall-provider/pcmall-provider-address/src/main/java/com/example/pcmallprovideraddress/dao/IAddressDao.java
@@ -0,0 +1,12 @@
+package com.example.pcmallprovideraddress.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.pcmallcommon.model.Address;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IAddressDao extends BaseMapper<Address> {
+    @Update("UPDATE address SET is_default=0,update_time=NOW() WHERE uid=#{uid} AND is_default=1;")
+    Integer cleanUserDefaultAddress(String uid);
+}
