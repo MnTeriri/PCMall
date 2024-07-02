7dbf261d3cd5e9fdfadff62d8a379fb87a828f99
Teriri <1511736399@qq.com>
2024-07-02 16:23:29 +0800

    PcmallProviderAddressApplication

--- pcmall-provider/pcmall-provider-address/src/main/java/com/example/pcmallprovideraddress/PcmallProviderAddressApplication.java
+++ pcmall-provider/pcmall-provider-address/src/main/java/com/example/pcmallprovideraddress/PcmallProviderAddressApplication.java
@@ -1,9 +1,13 @@
 package com.example.pcmallprovideraddress;
 
+import cn.hutool.extra.spring.EnableSpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableSpringUtil
 public class PcmallProviderAddressApplication {
 
     public static void main(String[] args) {
