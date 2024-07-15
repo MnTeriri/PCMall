554917e937905b6089e768fb9e592cbb4d54ff84
Teriri <1511736399@qq.com>
2024-07-15 16:43:36 +0800

    Initial Commit

--- pcmall-provider/pcmall-provider-brand/src/main/java/org/example/pcmallproviderbrand/PcmallProviderBrandApplication.java
+++ pcmall-provider/pcmall-provider-brand/src/main/java/org/example/pcmallproviderbrand/PcmallProviderBrandApplication.java
@@ -1,9 +1,13 @@
 package org.example.pcmallproviderbrand;
 
+import cn.hutool.extra.spring.EnableSpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableSpringUtil
 public class PcmallProviderBrandApplication {
 
     public static void main(String[] args) {
--- pcmall-provider/pcmall-provider-category/src/main/java/org/example/pcmallprovidercategory/PcmallProviderCategoryApplication.java
+++ pcmall-provider/pcmall-provider-category/src/main/java/org/example/pcmallprovidercategory/PcmallProviderCategoryApplication.java
@@ -1,9 +1,13 @@
 package org.example.pcmallprovidercategory;
 
+import cn.hutool.extra.spring.EnableSpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableSpringUtil
 public class PcmallProviderCategoryApplication {
 
     public static void main(String[] args) {
