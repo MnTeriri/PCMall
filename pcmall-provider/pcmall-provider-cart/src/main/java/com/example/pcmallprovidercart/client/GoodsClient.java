686358f404fd78acd9cbadd3f8ca257f1f4c8bd3
Teriri <1511736399@qq.com>
2024-07-18 17:42:45 +0800

    GoodsClient

--- pcmall-provider/pcmall-provider-cart/src/main/java/com/example/pcmallprovidercart/client/GoodsClient.java
+++ pcmall-provider/pcmall-provider-cart/src/main/java/com/example/pcmallprovidercart/client/GoodsClient.java
@@ -4,15 +4,17 @@ import com.example.pcmallcommon.model.Goods;
 import com.example.pcmallcommon.response.ResponseResult;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 
 @FeignClient(contextId = "goodsClient", value = "pcmall-provider-goods")
 public interface GoodsClient {
-    @PostMapping("/goods/searchGoodsById")
+    @PostMapping("/goods/newSearchGoodsById")
     ResponseResult<Goods> searchGoodsById(
-            @RequestParam("id") Integer id,
-            @RequestParam("isSearchCategory") boolean isSearchCategory,
-            @RequestParam("isSearchBrand") boolean isSearchBrand
+            @RequestBody Map<String, Boolean> aspectRule,
+            @RequestParam("id") Integer id
     );
 }
