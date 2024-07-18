2cd50031f931325b6092947c2d10a04abe5a961d
Teriri <1511736399@qq.com>
2024-07-18 17:43:25 +0800

    AutowiringSpringBeanJobFactory

--- pcmall-provider/pcmall-provider-order/src/main/java/com/example/pcmallproviderorder/util/AutowiringSpringBeanJobFactory.java
+++ pcmall-provider/pcmall-provider-order/src/main/java/com/example/pcmallproviderorder/util/AutowiringSpringBeanJobFactory.java
@@ -1,4 +1,4 @@
-package com.example.pcmallproviderpayment.util;
+package com.example.pcmallproviderorder.util;
 
 import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
