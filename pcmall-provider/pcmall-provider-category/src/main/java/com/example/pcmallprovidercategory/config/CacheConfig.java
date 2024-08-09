95e1d24e4d8ff8fb2367d47b8e1d4fb3931b4bab
Teriri <1511736399@qq.com>
2024-08-09 16:42:34 +0800

    CacheConfig

---
+++ pcmall-provider/pcmall-provider-category/src/main/java/com/example/pcmallprovidercategory/config/CacheConfig.java
@@ -0,0 +1,29 @@
+package com.example.pcmallprovidercategory.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Slf4j
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    public CacheConfig() {
+        log.debug("创建配置类对象：{}", this);
+    }
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        //自定义Spring Cache的RedisCacheConfiguration
+        //设置为Jackson序列化
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(5))//设置缓存有效期为5分钟
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+}
