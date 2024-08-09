e8bae1c5ec843942694e3a1fadf9a5591d12f25d
Teriri <1511736399@qq.com>
2024-08-09 16:43:44 +0800

    CartServiceAspect

--- pcmall-provider/pcmall-provider-cart/src/main/java/com/example/pcmallprovidercart/aspect/CartServiceAspect.java
+++ pcmall-provider/pcmall-provider-cart/src/main/java/com/example/pcmallprovidercart/aspect/CartServiceAspect.java
@@ -32,12 +32,31 @@ public class CartServiceAspect {
         log.debug("创建切面类对象：{}", this);
     }
 
-    @Pointcut("execution(* com.example.pcmallprovidercart.service.ICartService.searchCartList(..))")
-    public void pointCut() {
+    @Around("execution(* com.example.pcmallprovidercart.service.ICartService.searchCartById(..))")
+    public Object searchCartAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.debug("进入切面");
+        log.debug("方法参数：{}", Arrays.toString(joinPoint.getArgs()));
+        log.debug("方法签名：{}", joinPoint.getSignature());
+        log.debug("执行。。。。");
+        Cart result = (Cart) joinPoint.proceed();
+        log.debug("结果类名：{}", result.getClass());
+        log.debug("结果：{}", result);
+        if (result == null) {
+            return null;
+        }
+        CompletableFuture<Void> completableFuture = CompletableFuture
+                .supplyAsync(() -> goodsClient.searchGoodsById(new HashMap<>() {{
+                    put("isSearchBrand", true);
+                    put("isSearchCategory", true);
+                }}, result.getGid()).getData(), threadPoolTaskExecutor)
+                .thenAccept(result::setGoods);
+        completableFuture.join();
+        log.debug("增强后结果：{}", result);
+        return result;
     }
 
-    @Around("pointCut()")
-    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("execution(* com.example.pcmallprovidercart.service.ICartService.searchCartList(..))")
+    public Object searchCartListAround(ProceedingJoinPoint joinPoint) throws Throwable {
         log.debug("进入切面");
         log.debug("方法参数：{}", Arrays.toString(joinPoint.getArgs()));
         log.debug("方法签名：{}", joinPoint.getSignature());
@@ -50,9 +69,9 @@ public class CartServiceAspect {
         for (Cart cart : list) {
             //设置异步任务
             CompletableFuture<Void> completableFuture = CompletableFuture
-                    .supplyAsync(() -> goodsClient.searchGoodsById(new HashMap<>(){{
-                        put("isSearchBrand",true);
-                        put("isSearchCategory",true);
+                    .supplyAsync(() -> goodsClient.searchGoodsById(new HashMap<>() {{
+                        put("isSearchBrand", true);
+                        put("isSearchCategory", true);
                     }}, cart.getGid()).getData(), threadPoolTaskExecutor)
                     .thenAccept(cart::setGoods);
             futures.add(completableFuture);
