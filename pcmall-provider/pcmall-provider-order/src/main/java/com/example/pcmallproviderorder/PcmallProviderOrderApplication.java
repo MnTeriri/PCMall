90fd3635627f6f0f73cff0c7d0847dc6fb5d8197
Teriri <1511736399@qq.com>
2024-08-16 19:43:32 +0800

    Spring启动类

--- pcmall-provider/pcmall-provider-order/src/main/java/com/example/pcmallproviderorder/PcmallProviderOrderApplication.java
+++ pcmall-provider/pcmall-provider-order/src/main/java/com/example/pcmallproviderorder/PcmallProviderOrderApplication.java
@@ -4,9 +4,11 @@ import cn.hutool.extra.spring.EnableSpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.example.pcmallcommon.client")
 @EnableSpringUtil
 public class PcmallProviderOrderApplication {
 
--- pcmall-provider/pcmall-provider-storage/src/main/java/com/example/pcmallproviderstorage/PcmallProviderStorageApplication.java
+++ pcmall-provider/pcmall-provider-storage/src/main/java/com/example/pcmallproviderstorage/PcmallProviderStorageApplication.java
@@ -1,5 +1,6 @@
 package com.example.pcmallproviderstorage;
 
+import cn.hutool.extra.spring.EnableSpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,7 +8,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.example.pcmallcommon.client")
+@EnableSpringUtil
 public class PcmallProviderStorageApplication {
 
     public static void main(String[] args) {
