b313d6f133709b54300b61445e8db8454352c088
Teriri <1511736399@qq.com>
2024-08-16 19:45:08 +0800

    RedisConfig

--- pcmall-provider/pcmall-provider-brand/src/main/java/com/example/pcmallproviderbrand/config/RedisConfig.java
+++ pcmall-provider/pcmall-provider-brand/src/main/java/com/example/pcmallproviderbrand/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.example.pcmallproviderbrand.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
