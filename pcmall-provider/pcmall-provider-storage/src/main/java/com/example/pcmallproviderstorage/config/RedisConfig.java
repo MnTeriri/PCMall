dcd02b16acac639703d2676d28ddc8434d3a5d0b
Teriri <1511736399@qq.com>
2024-08-28 18:28:02 +0800

    Config

--- pcmall-provider/pcmall-provider-storage/src/main/java/com/example/pcmallproviderstorage/config/RedisConfig.java
+++ pcmall-provider/pcmall-provider-storage/src/main/java/com/example/pcmallproviderstorage/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.example.pcmallproviderstorage.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,7 +10,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Slf4j
 @Configuration
-@EnableCaching
 public class RedisConfig {
     public RedisConfig() {
         log.debug("创建配置类对象：RedisConfig");
